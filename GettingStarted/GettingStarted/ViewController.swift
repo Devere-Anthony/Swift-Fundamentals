da13cb8e161efc93bf4315525b3c8ef35a2fc3eb
daw <daw-research@protonmail.com>
2023-12-11 13:55:42 -0500

    build getting start application to test physical and simulation devices

---
+++ GettingStarted/GettingStarted/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GettingStarted
+//
+//  Created by Devere Weaver on 12/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
