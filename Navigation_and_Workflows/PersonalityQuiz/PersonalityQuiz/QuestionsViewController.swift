ee67fb6ac784e820897f3ff1f5e2d8fe816c84bc
daw <daw-research@protonmail.com>
2023-12-22 14:26:24 -0500

    completed Swift Fundamentals

--- Navigation_and_Workflows/PersonalityQuiz/PersonalityQuiz/QuestionsViewController.swift
+++ Navigation_and_Workflows/PersonalityQuiz/PersonalityQuiz/QuestionsViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class QuestionsViewController: UIViewController {
     
+    // displays the questions as the questions array is traversed
     @IBOutlet var questionLabel: UILabel!
     
     // single stack interface outlets
@@ -20,20 +21,34 @@ class QuestionsViewController: UIViewController {
     
     // slider stack interface outlets (missing slider and submit button)
     @IBOutlet var sliderStackView: UIStackView!
+    @IBOutlet var slider: UISlider!
     @IBOutlet var sliderStackLabel1: UILabel!
     @IBOutlet var sliderStackLabel2: UILabel!
     
     // multiple stack interface outlets (missing submit button and sliders?)
     @IBOutlet var multipleStackView: UIStackView!
     @IBOutlet var multiLabel1: UILabel!
+    @IBOutlet var multiSwitch1: UISwitch!
     @IBOutlet var multiLabel2: UILabel!
+    @IBOutlet var multiSwitch2: UISwitch!
     @IBOutlet var multiLabel3: UILabel!
+    @IBOutlet var multiSwitch3: UISwitch!
     @IBOutlet var multiLabel4: UILabel!
+    @IBOutlet var multiSwitch4: UISwitch!
     
+    /* this method returns an optional ResultsViewController */
+    @IBSegueAction func showResults(_ coder: NSCoder) -> ResultsViewController? {
+        /* we must initialize a ResultsViewController in this method and then return it,
+         * to do so, return it with the custom initializer defined in its view controller
+         * file 
+         */
+        return ResultsViewController(coder: coder, responses: answersChosen)
+    }
     @IBOutlet var questionProgressView: UIProgressView!
     
     /* VARIBLES */
     var questionIndex = 0    // keep track of the number of questions asked
+    var answersChosen: [Answer] = []
     var questions: [Question] = [             // array of Questions which will be displayed to the user
         Question(
             text: "Which food do you like the most?",
@@ -64,7 +79,12 @@ class QuestionsViewController: UIViewController {
                 Answer(text: "I barely notice them", type: .turtle),
                 Answer(text: "I love them", type: .dog)
             ])
-        ]   // end questions j
+        ]   // end questions
+    
+    
+    // CURRENT GOAL: record the user response in the single stack view based on the
+    // type of button they pressed, then once the user response has been recorded
+    // we can move to the next question and display it
     
     /* METHODS */
     override func viewDidLoad() {
@@ -125,7 +145,16 @@ class QuestionsViewController: UIViewController {
         * Args:
         *      1. answers: [Answers] - an array of Answer struct instances
         */
+        
         multipleStackView.isHidden = false
+        
+        // update controls to default positions each display
+        multiSwitch1.isOn = false
+        multiSwitch2.isOn = false
+        multiSwitch3.isOn = false
+        multiSwitch4.isOn = false
+
+        // display text
         multiLabel1.text = answers[0].text
         multiLabel2.text = answers[1].text
         multiLabel3.text = answers[2].text
@@ -142,11 +171,106 @@ class QuestionsViewController: UIViewController {
         * the number of answers isn't guaranteed. This will prevent out of bounds errors.
         */
         sliderStackView.isHidden = false
+        slider.setValue(0.5, animated: true)
         sliderStackLabel1.text = answers.first?.text
         sliderStackLabel2.text = answers.last?.text
     }
     
-    
-    
+    @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
+        /* singleAnswerButtonPressed() - action taken when the user pressed an
+         * answer button the single repsonse question view; it will record the
+         * user response in the answerChosen array
+         *
+         * Args: N/A
+         */
+        
+        // get all the answers for the current question into this array
+        let currentAnswers = questions[questionIndex].answers
 
+        // specify which button triggered the method and append its
+        // answer to the answerChosen array
+        switch sender {
+        case singleStackButton1:
+            answersChosen.append(currentAnswers[0])
+        case singleStackButton2:
+            answersChosen.append(currentAnswers[1])
+        case singleStackButton3:
+            answersChosen.append(currentAnswers[2])
+        case singleStackButton4:
+            answersChosen.append(currentAnswers[3])
+        default:
+            break
+        }
+        
+        print("After single answer question: \(answersChosen)")
+        nextQuestion()
+        
+    }
+    
+    
+    @IBAction func multipleAnswerButtonPressed() {
+        /* multipleAnswerButtonPressed() - action taken when the submit answers button
+         * is pressed on the multiple response question view; the labels are added to
+         * the answers response if their corresponding switch is toggled on
+         *
+         * Args: N/A
+         */
+        
+        // check the state of all four switches
+        // if the state is "On" then add the corresponding Answer to the chosenAnswers array
+        // if the state is "Off", do nothing
+        
+        // store array of answers for current Question in local scope
+        let currentAnswers = questions[questionIndex].answers
+        
+        if multiSwitch1.isOn {
+            answersChosen.append(currentAnswers[0])
+        }
+        if multiSwitch2.isOn {
+            answersChosen.append(currentAnswers[1])
+        }
+        if multiSwitch3.isOn {
+            answersChosen.append(currentAnswers[2])
+        }
+        if multiSwitch4.isOn {
+            answersChosen.append(currentAnswers[3])
+        }
+        
+        print("After multiple answers button: \(answersChosen)")
+        nextQuestion()
+    }
+    
+    @IBAction func sliderAnswerButtonPressed() {
+        /* read the current position of the slider and use this reading to
+         * determine which answer to add to the collection
+         */
+        let currentAnswers = questions[questionIndex].answers
+        print(slider.value)
+
+        if slider.value < 0.24 {
+            answersChosen.append(currentAnswers[0])
+        } else if slider.value > 0.24 && slider.value < 0.49 {
+            answersChosen.append(currentAnswers[1])
+        } else if slider.value > 0.49 && slider.value < 0.74 {
+            answersChosen.append(currentAnswers[2])
+        } else if slider.value > 0.74 {
+            answersChosen.append(currentAnswers[3])
+        }
+        
+        print("After slider question: \(answersChosen)")
+        nextQuestion()
+    }
+    
+    
+    func nextQuestion() {
+        /* increment the next question counter */
+        questionIndex += 1
+        
+        if questionIndex < questions.count {
+            updateUI()    // display any remaining questions
+        } else {    // move to the results view
+            performSegue(withIdentifier: "Results", sender: nil)
+        }
+    }
+    
 }
--- Navigation_and_Workflows/PersonalityQuiz/PersonalityQuiz/ResultsViewController.swift
+++ Navigation_and_Workflows/PersonalityQuiz/PersonalityQuiz/ResultsViewController.swift
@@ -8,11 +8,52 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
+    
+    
+    @IBOutlet var resultAnswerLabel: UILabel!
+    @IBOutlet var resultDefinitionLabel: UILabel!
+    var responses: [Answer]   // store the user responses from the Question view
+                              // must be assigned an initial value here or in a
+                              // custom initializer
 
+    init?(coder: NSCoder, responses: [Answer]) {
+        /* coder is used by UIKit to initialize the view controller from the Storyboard */
+        self.responses = responses
+        super.init(coder: coder)
+        navigationItem.hidesBackButton = true
+    }
+    
+    /* when you create a custom initializer you must also implement any required  initializers
+     * that the superclass defines, fortunately Xcode provides a "Fix" button to do so
+     */
+    
+   required init?(coder: NSCoder) {
+       /* crash if the default initializer is called */
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        calculatePersonalityResult()
 
-        // Do any additional setup after loading the view.
+    }
+    
+    func calculatePersonalityResult() {
+        // combine the items in an array by iterating over them
+        // and peforming the code inside the brackets
+        let frequencyOfAnswers = responses.reduce(into: [:]) {
+            (counts, answer) in
+            counts[answer.type, default: 0] += 1
+        }
+        
+        let frequentAnswersSorted = frequencyOfAnswers.sorted(by: {(pair1, pair2) in
+            return pair1.value > pair2.value
+        })
+        
+        let mostCommonAnswer = frequencyOfAnswers.first!.key
+        
+        resultAnswerLabel.text = "You are a \(mostCommonAnswer.rawValue)!"
+        resultDefinitionLabel.text = mostCommonAnswer.definition
     }
     
 }
